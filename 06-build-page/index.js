ad2acd3b86a4c2b16ea867a1c8ba2d9dcf9e8fac
sem0ark <arcadii.sem@gmail.com>
2024-01-18 21:32:37 +0100

    feat: task 6 add copy folder

--- 06-build-page/index.js
+++ 06-build-page/index.js
@@ -6,20 +6,36 @@ function localPath(...ps) {
 }
 
 // modified version of https://gist.github.com/lovasoa/8691344?permalink_comment_id=3299089#gistcomment-3299089
-async function* walk_filtered(dir, condition = () => true) {
+async function* walk(dir) {
   for await (const d of await fs.promises.opendir(dir)) {
     const p = path.join(dir, d.name);
 
-    if (d.isDirectory()) yield* walk_filtered(p);
-    else if (d.isFile() && condition(p)) yield p;
+    yield p;
+    if (d.isDirectory()) yield* walk(p);
   }
 }
 
-async function prepareHTML() {}
+async function copyFolder(from, to) {
+  try {
+    await fs.promises.rmdir(to, { recursive: true, force: true });
+  } finally {
+    await fs.promises.mkdir(to);
+  }
+
+  for await (const p of walk(from)) {
+    const s = await fs.promises.stat(p);
+    if (s.isDirectory()) fs.promises.mkdir(p);
+    else {
+      fs.createReadStream(p).pipe(
+        fs.createWriteStream(path.join(to, path.basename(p))),
+      );
+    }
+  }
+}
+
+async function prepareHTML(template, components) {}
 
 async function bundleCSS() {}
-
-async function copyFolder() {}
 
 async function bundle() {
   const result_folder = localPath('project-dist');
@@ -36,3 +52,5 @@ async function bundle() {
     ]);
   }
 }
+
+bundle();
